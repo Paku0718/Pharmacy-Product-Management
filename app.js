365550ef2454269891649ab9717849b0dc860c2e
Mukta-28 <mukta3725@gmail.com>
2025-05-07 21:45:39 +0530

    Created the main server entry point and product routes setup

---
+++ app.js
@@ -0,0 +1,13 @@
+//middleware setup
+const express = require('express');
+const cors = require('cors');
+const productRoutes = require('./routes/productRoutes');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/products', productRoutes);
+
+module.exports = app;
